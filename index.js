709219368e0cbd46175c624121e996689659fc1e
jasnmar <jasnmar@gmail.com>
2024-03-30 12:32:07 -0400

    Challenge 3 start

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-let myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to Clean Code"]
-
-// Create a function that takes a single parameter, an array,
-// and logs all the items of the array to the console.
-// Call the function while passing in myCourses as an argument
-
-function printArray(array) {
-    for (let i=0; i<array.length; i++){
-        console.log(array[i]);
-    }
-}
-printArray(myCourses);
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
